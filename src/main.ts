4d65ccdc5aef0200851539dc69a587dfe669fd55
Leng <horleng123@gmail.com>
2026-09-23 14:25:54 +0700

    :apple: Fix styling issue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./app/styles/index.scss"
-import "./app/styles/ios-style.scss"
 import "./app/styles/blog-style.scss"
 // import 'highlight.js/styles/stackoverflow-light.css';
 import router from "./app/route.config"
